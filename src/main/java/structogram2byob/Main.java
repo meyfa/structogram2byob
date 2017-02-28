72d6bd1c51bf9cb903dd0177e48ba57fb7833332
JangoBrick <mail@jangobrick.de>
2017-02-28 15:01:17 +0100

    Setup Eclipse workspace

---
+++ src/main/java/structogram2byob/Main.java
@@ -0,0 +1,17 @@
+package structogram2byob;
+
+/**
+ * Application entry point.
+ */
+public class Main
+{
+    /**
+     * Application entry point.
+     * 
+     * @param args The command-line arguments.
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
