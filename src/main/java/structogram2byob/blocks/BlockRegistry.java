04ec88aef3bd7ea560677589802f15e288bb914d
JangoBrick <mail@jangobrick.de>
2017-03-04 20:31:50 +0100

    Add FunctionBlock and BlockRegistry

---
+++ src/main/java/structogram2byob/blocks/BlockRegistry.java
@@ -0,0 +1,74 @@
+package structogram2byob.blocks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+/**
+ * Mapping of block descriptions to the respective block, with the ability to
+ * look blocks up with respect to
+ * {@link BlockDescription#isAssignableFrom(BlockDescription)}.
+ */
+public class BlockRegistry
+{
+    private final BlockRegistry base;
+    private final Map<BlockDescription, Block> blocks = new HashMap<>();
+
+    /**
+     * Constructs an empty block registry.
+     */
+    public BlockRegistry()
+    {
+        this(null);
+    }
+
+    /**
+     * Constructs a block registry that will extend the given base registry
+     * without modifying it.
+     * 
+     * @param base The block registry to be used as the base for this one.
+     */
+    public BlockRegistry(BlockRegistry base)
+    {
+        this.base = base;
+    }
+
+    /**
+     * Registers the given block with this registry.
+     * 
+     * @param block The block to register.
+     * 
+     * @throws IllegalArgumentException If the block is already registered.
+     */
+    public void register(Block block)
+    {
+        if (blocks.containsKey(block.getDescription())) {
+            throw new IllegalArgumentException("block already registered");
+        }
+        blocks.put(block.getDescription(), block);
+    }
+
+    /**
+     * Returns the block matching the given block description as per
+     * {@link BlockDescription#isAssignableFrom(BlockDescription)}.
+     * 
+     * @param desc The description to look up.
+     * @return The block matching the given description.
+     */
+    public Block lookup(BlockDescription desc)
+    {
+        Block baseLookup = base != null ? base.lookup(desc) : null;
+        if (baseLookup != null) {
+            return baseLookup;
+        }
+
+        for (Entry<BlockDescription, Block> e : blocks.entrySet()) {
+            if (e.getKey().isAssignableFrom(desc)) {
+                return e.getValue();
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/structogram2byob/blocks/FunctionBlock.java
@@ -0,0 +1,51 @@
+package structogram2byob.blocks;
+
+import java.util.List;
+
+import scratchlib.objects.fixed.collections.ScratchObjectArray;
+import scratchlib.objects.fixed.data.ScratchObjectSymbol;
+import structogram2byob.ScratchType;
+import structogram2byob.program.expressions.Expression;
+
+
+/**
+ * A function block is the standard extension of the block class, used for
+ * everything except structural or highly specific blocks.
+ * 
+ * <p>
+ * Blocks constructed this way have the usual description and return value, and
+ * otherwise only consist of a method they call with some number of parameters.
+ */
+public class FunctionBlock extends Block
+{
+    private final String method;
+
+    /**
+     * Constructs a new function block with the given description, return type,
+     * and Scratch method.
+     * 
+     * @param desc The block description.
+     * @param returnType The type of value the block returns. May be null.
+     * @param method The Scratch method this block calls.
+     */
+    public FunctionBlock(BlockDescription desc, ScratchType returnType,
+            String method)
+    {
+        super(desc, returnType);
+        this.method = method;
+    }
+
+    @Override
+    public ScratchObjectArray toScratch(List<Expression> params)
+    {
+        ScratchObjectArray a = new ScratchObjectArray();
+
+        a.add(new ScratchObjectSymbol(method));
+
+        for (Expression e : params) {
+            a.add(e.toScratch());
+        }
+
+        return a;
+    }
+}
